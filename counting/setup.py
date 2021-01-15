8735e4bcced610aa82c6b9431e599fc16d8af538
SandPhoeniXX <60070961+SandPhoenixX517@users.noreply.github.com>
2021-01-15 21:37:45 +0100

    Create setup.py

---
+++ counting/setup.py
@@ -0,0 +1,18 @@
+import os 
+
+os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_Counting_Columbus_CSUAV_AFRL.tbz")
+os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_Counting_Potsdam_ISPRS.tbz")
+#os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_Counting_Selwyn_LINZ.tbz")
+#os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_Counting_Toronto_ISPRS.tbz")
+#os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_Counting_Vaihingen_ISPRS.tbz")
+#os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_Counting_Utah_AGRC.tbz")
+os.system("tar -xvjf ./COWC_Counting_Columbus_CSUAV_AFRL.tbz")
+os.system("tar -xvjf ./COWC_Counting_Potsdam_ISPRS.tbz")
+os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_test_list_64_class.txt.bz2")
+os.system("wget https://gdo152.llnl.gov/cowc/download/cowc/datasets/patch_sets/counting/COWC_train_list_64_class.txt.bz2")
+os.system("bzip2 -d COWC_test_list_64_class.txt.bz2")
+os.system("bzip2 -d COWC_train_list_64_class.txt.bz2")
+os.system("cp COWC_test_list_64_class.txt ./Columbus_CSUAV_AFRL/")
+os.system("cp COWC_train_list_64_class.txt ./Potsdam_ISPRS/")
+os.system("cp COWC_train_list_64_class.txt ./Columbus_CSUAV_AFRL/")
+os.system("cp COWC_test_list_64_class.txt ./Potsdam_ISPRS/")
